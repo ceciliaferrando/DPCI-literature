891466ac76bf91007d1580332697aaa0c5c628ff
ceciliaferrando <cferrando@umass.edu>
2021-01-10 19:49:38 -0500

    experiment driver

--- run.py
+++ run.py
@@ -2,36 +2,42 @@ import numpy as np
 import argparse
 from karwavadhan import *
 
+parser = argparse.ArgumentParser(description='DP CI reference algorithms')
+parser.add_argument('--alg', type=str, default='vadhan', help='vadhan or brawner or dorazio')
+parser.add_argument('--n', type=int, default=10000, help='number of data points')
+parser.add_argument('--e', type=float, default=0.1, help='privacy parameter epsilon')
+parser.add_argument('--r', type=float, default=32,
+                    help="Upper bound of the data range. Range symmetric with respect to 0. Lowerbound set to minus this")
+parser.add_argument('--a', type=float, default=0.05, help='significance level')
+parser.add_argument('--iters', type=int, default=1000, help='number of iterations for computing CI coverage')
+parser.add_argument('--center', type=float, default=0.0, help='significance level')
+args = parser.parse_args()
+
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='DP CI reference algorithms')
-    parser.add_argument('alg', type=str, help='vadhan or brawner or dorazio')
-    parser.add_argument('n', type=int, help='number of data points')
-    parser.add_argument('e', type=float, help='privacy parameter epsilon')
-    parser.add_argument('e', type=float, help='privacy parameter epsilon')
+    alg = args.alg
+    true_param = args.center
+    n = args.n
+    e = args.e
+    r = args.r
+    a = args.a
+    iters = args.iters
 
-    true_param = 0
-    n_experiments = 500
-    is_true_param_covered = [0]*n_experiments
-    moes = [0]*n_experiments
+    is_true_param_covered = [0]*iters
+    moes = [0]*iters
 
-    for experiment in range(n_experiments):
+    for iter in range(iters):
 
-        n = 10000
-        #db = np.random.normal(0, 1, n)
-        #np.save("db.csv", db)
-        #db = np.array([0, 0.2, 0.1, 0.05, -0.05, -1])
-        db = np.loadtxt("db.txt")
-        a = 0.05    #significance level
-        e = 0.1
-        stdmin, stdmax = 0.2, 5 #see original code
-        r = 32                  #check how they do it in the paper
-        xmin, xmax = -r, r
-        ci = karwa_vadhan_ci(db, a, e, stdmin, stdmax, xmin, xmax)
-        if ci[0] <= true_param <= ci[1]:
-            is_true_param_covered[experiment] = 1
-        moe = (ci[1]-ci[0])/2
-        moes[experiment] = moe
+        db = np.random.normal(0, 1, n)
+
+        if alg == 'vadhan':
+            stdmin, stdmax = 0.2, 5 #see original code
+            xmin, xmax = -r, r
+            ci = karwa_vadhan_ci(db, a, e, stdmin, stdmax, xmin, xmax)
+            if ci[0] <= true_param <= ci[1]:
+                is_true_param_covered[iter] = 1
+            moe = (ci[1]-ci[0])/2
+            moes[iter] = moe
 
     print("mean MoE", np.mean(moes))
     print("coverage", np.mean(is_true_param_covered))
